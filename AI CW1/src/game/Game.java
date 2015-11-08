d04c3c49a0eb8b3ec004dff9d345fda121c1d519
Novanet- <gamemaster8888@gmail.com>
2015-11-08 15:08:25 +0000

    Implemented skeleton for running the game

--- AI CW1/src/game/Game.java
+++ AI CW1/src/game/Game.java
@@ -3,6 +3,8 @@
  */
 package game;
 
+import java.awt.Rectangle;
+import java.util.ArrayList;
 
 /**
  * @author Will
@@ -10,13 +12,30 @@ package game;
  */
 public class Game
 {
+	BoardState boardstate;
 
 	/**
 	 * 
 	 */
 	public Game()
 	{
-		// TODO Auto-generated constructor stub
+		Rectangle board = null;
+		//define board dimensions
+		ArrayList<BoardPiece> pieces = null;
+		//add board pieces
+		boardstate = new BoardState(board, pieces);
+	}
+	
+	public GameResult runGame(BoardState initBoardState, SearchType search)
+	{
+		GameResult gameResult = null;
+		switch (search)
+        {
+			case DEPTH_FIRST:
+				return gameResult;
+			default:
+				return gameResult;
+        }
 	}
 
 }
