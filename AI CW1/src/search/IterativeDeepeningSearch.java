63181d62308a55a62243857117179f4fda52cac1
Novanet- <gamemaster8888@gmail.com>
2015-11-14 19:41:43 +0000

    NEarly fixed iterative depening

--- AI CW1/src/search/IterativeDeepeningSearch.java
+++ AI CW1/src/search/IterativeDeepeningSearch.java
@@ -103,7 +103,7 @@ public class IterativeDeepeningSearch extends Search
 				if (currentLevel < maxDepth)
 				{
 					currentLevel++;
-					nodeCounter++;
+					nodeCounter++; 
 					currentNode = expandRandomChild(nextMoves);
 				}
 				else
